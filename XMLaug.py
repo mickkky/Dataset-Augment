40d04ef518881d72c4b8438fce730bf98e9c2206
mickkky <641401353@qq.com>
2018-11-25 15:41:41 +0800

    bounding box loop

--- XMLaug.py
+++ XMLaug.py
@@ -4,13 +4,11 @@ import os
 from os import getcwd
 import numpy as np
 from PIL import Image
-import cv2
 
 import imgaug as ia
 from imgaug import augmenters as iaa
 
 ia.seed(1)
-
 
 def read_xml_annotation(root, image_id):
     in_file = open(os.path.join(root, image_id))
@@ -27,16 +25,9 @@ def read_xml_annotation(root, image_id):
         ymax = int(bndbox.find('ymax').text)
         # print(xmin,ymin,xmax,ymax)
         bndboxlist.append([xmin,ymin,xmax,ymax])
-        print(bndboxlist)
+        # print(bndboxlist)
 
     bndbox = root.find('object').find('bndbox')
-
-    xmin = int(bndbox.find('xmin').text)
-    xmax = int(bndbox.find('xmax').text)
-    ymin = int(bndbox.find('ymin').text)
-    ymax = int(bndbox.find('ymax').text)
-
-    # return (xmin, ymin, xmax, ymax)
     return bndboxlist
 # (506.0000, 330.0000, 528.0000, 348.0000) -> (520.4747, 381.5080, 540.5596, 398.6603)
 def change_xml_annotation(root, image_id, new_target):
@@ -60,110 +51,128 @@ def change_xml_annotation(root, image_id, new_target):
     ymax.text = str(new_ymax)
     tree.write(os.path.join(root, str(image_id) + "_aug" + '.xml'))
 
+def change_xml_list_annotation(root, image_id, new_target,saveroot,id):
+
+    in_file = open(os.path.join(root, str(image_id) + '.xml'))  # 这里root分别由两个意思
+    tree = ET.parse(in_file)
+    xmlroot = tree.getroot()
+    index = 0
+
+    for object in xmlroot.findall('object'):  # 找到root节点下的所有country节点
+        bndbox = object.find('bndbox')  # 子节点下节点rank的值
+
+        # xmin = int(bndbox.find('xmin').text)
+        # xmax = int(bndbox.find('xmax').text)
+        # ymin = int(bndbox.find('ymin').text)
+        # ymax = int(bndbox.find('ymax').text)
+
+        new_xmin = new_target[index][0]
+        new_ymin = new_target[index][1]
+        new_xmax = new_target[index][2]
+        new_ymax = new_target[index][3]
+
+        xmin = bndbox.find('xmin')
+        xmin.text = str(new_xmin)
+        ymin = bndbox.find('ymin')
+        ymin.text = str(new_ymin)
+        xmax = bndbox.find('xmax')
+        xmax.text = str(new_xmax)
+        ymax = bndbox.find('ymax')
+        ymax.text = str(new_ymax)
+
+        index = index + 1
+
+    tree.write(os.path.join(saveroot, str(image_id) + "_aug_" + str(id) + '.xml'))
+
+
+def mkdir(path):
+
+    # 去除首位空格
+    path = path.strip()
+    # 去除尾部 \ 符号
+    path = path.rstrip("\\")
+    # 判断路径是否存在
+    # 存在     True
+    # 不存在   False
+    isExists = os.path.exists(path)
+    # 判断结果
+    if not isExists:
+        # 如果不存在则创建目录
+         # 创建目录操作函数
+        os.makedirs(path)
+        print(path + ' 创建成功')
+        return True
+    else:
+        # 如果目录存在则不创建，并提示目录已存在
+        print(path + ' 目录已存在')
+        return False
 
 if __name__ == "__main__":
-    cmd = os.getcwd()
-    # cmd = "C:\\Users\\64140\Desktop\\500\Example"
-    # cmd = "/Users/wangbenkang/Desktop/500/Example"
-    image_id = "000010"
-    img = Image.open(os.path.join(cmd, str(image_id) + '.jpg'))
-    img = np.array(img)
 
-    bndbox = read_xml_annotation(cmd, str(image_id) + '.xml')
+    IMG_DIR = "path/to/your/IMG"
+    XML_DIR = "path/to/your/XML"
+
+    AUG_XML_DIR = "path/to/your/AUG_XML" # 存储增强后的XML文件夹路径
+    mkdir(AUG_XML_DIR)
+
+    AUG_IMG_DIR = "path/to/your/AUG_IMG" # 存储增强后的影像文件夹路径
+    mkdir(AUG_IMG_DIR)
+
+    AUGLOOP = 10 # 每张影像增强的数量
 
     boxes_img_aug_list = []
+    new_bndbox = []
+    new_bndbox_list = []
+
 
     # 影像增强
     seq = iaa.Sequential([
         iaa.Flipud(0.5),  # vertically flip 20% of all images
+        iaa.Fliplr(0.5),  # 镜像
         iaa.Multiply((1.2, 1.5)),  # change brightness, doesn't affect BBs
+        iaa.GaussianBlur(sigma=(0, 3.0)), # iaa.GaussianBlur(0.5),
         iaa.Affine(
-            translate_px={"x": 10, "y": 10},
+            translate_px={"x": 15, "y": 15},
             scale=(0.8, 0.95),
-            rotate=(-10, 10)
+            rotate=(-30, 30)
         )  # translate by 40/60px on x/y axis, and scale to 50-70%, affects BBs
     ])
-    seq_det = seq.to_deterministic()  # 保持坐标和图像同步改变，而不是随机
-    image_aug = seq_det.augment_images([img])
 
-    # bndbox 坐标增强
-    for i in range(len(bndbox)):
-        bbs = ia.BoundingBoxesOnImage([
-            ia.BoundingBox(x1=bndbox[i][0], y1=bndbox[i][1], x2=bndbox[i][2], y2=bndbox[i][3]),
-        ], shape=img.shape)
+    for root, sub_folders, files in os.walk(XML_DIR):
 
-        bbs_aug = seq_det.augment_bounding_boxes([bbs])
-        boxes_img_aug_list.append(bbs_aug)
+        for name in files:
 
+            bndbox = read_xml_annotation(XML_DIR, name)
 
+            for epoch in range(AUGLOOP):
+                seq_det = seq.to_deterministic()  # 保持坐标和图像同步改变，而不是随机
 
-# 循环
-    for i in range(len(bndbox)):
-        bbs = ia.BoundingBoxesOnImage([
-            # ia.BoundingBox(x1=bndbox[0], y1=bndbox[1], x2=bndbox[2], y2=bndbox[3]) #改这里！
-            ia.BoundingBox(x1=bndbox[i][0], y1=bndbox[i][1], x2=bndbox[i][2], y2=bndbox[i][3]),  # for i in len(bndbox)
-            # ia.BoundingBox(x1=bndbox[1][0], y1=bndbox[1][1], x2=bndbox[1][2], y2=bndbox[1][3])
-        ], shape=img.shape)
-        seq = iaa.Sequential([
-            iaa.Flipud(0.5),  # vertically flip 20% of all images
-            iaa.Multiply((1.2, 1.5)),  # change brightness, doesn't affect BBs
-            iaa.Affine(
-                translate_px={"x": 10, "y": 10},
-                scale=(0.8, 0.95),
-                rotate=(-10, 10)
-            )  # translate by 40/60px on x/y axis, and scale to 50-70%, affects BBs
-        ])
-        seq_det = seq.to_deterministic()  # 保持坐标和图像同步改变，而不是随机
-        image_aug = seq_det.augment_images([img])[0]
-        bbs_aug = seq_det.augment_bounding_boxes([bbs])[0]
-        boxes_img_aug.append(bbs_aug)
+                # 读取图片
+                img = Image.open(os.path.join(IMG_DIR, name[:-4] + '.jpg'))
+                img = np.array(img)
 
+                # bndbox 坐标增强
+                for i in range(len(bndbox)):
+                    bbs = ia.BoundingBoxesOnImage([
+                        ia.BoundingBox(x1=bndbox[i][0], y1=bndbox[i][1], x2=bndbox[i][2], y2=bndbox[i][3]),
+                    ], shape=img.shape)
 
-    bbs = ia.BoundingBoxesOnImage([
-        # ia.BoundingBox(x1=bndbox[0], y1=bndbox[1], x2=bndbox[2], y2=bndbox[3]) #改这里！
-        ia.BoundingBox(x1=bndbox[0][0], y1=bndbox[0][1], x2=bndbox[0][2], y2=bndbox[0][3]), #for i in len(bndbox)
-        # ia.BoundingBox(x1=bndbox[1][0], y1=bndbox[1][1], x2=bndbox[1][2], y2=bndbox[1][3])
-    ], shape=img.shape)
-    # boxes_img_aug.append(bbs)
+                    bbs_aug = seq_det.augment_bounding_boxes([bbs])[0]
+                    boxes_img_aug_list.append(bbs_aug)
 
-    bbs1 = ia.BoundingBoxesOnImage([
-        # ia.BoundingBox(x1=bndbox[0], y1=bndbox[1], x2=bndbox[2], y2=bndbox[3]) #改这里！
-        ia.BoundingBox(x1=bndbox[1][0], y1=bndbox[1][1], x2=bndbox[1][2], y2=bndbox[1][3])
-    ], shape=img.shape)
-    # boxes_img_aug.append(bbs1)
+                    # new_bndbox_list:[[x1,y1,x2,y2],...[],[]]
+                    new_bndbox_list.append([int(bbs_aug.bounding_boxes[0].x1),
+                                            int(bbs_aug.bounding_boxes[0].y1),
+                                            int(bbs_aug.bounding_boxes[0].x2),
+                                            int(bbs_aug.bounding_boxes[0].y2)])
+                # 存储变化后的图片
+                image_aug = seq_det.augment_images([img])[0]
+                path = os.path.join(AUG_IMG_DIR, str(name[:-4]) + "_aug_" + str(epoch) + '.jpg')
+                image_auged = bbs.draw_on_image(image_aug, thickness=0)
+                Image.fromarray(image_auged).save(path)
 
-    seq = iaa.Sequential([
-        iaa.Flipud(0.5),  # vertically flip 20% of all images
-        iaa.Multiply((1.2, 1.5)),  # change brightness, doesn't affect BBs
-        iaa.Affine(
-            translate_px={"x": 10, "y": 10},
-            scale=(0.8, 0.95),
-            rotate=(-10, 10)
-        )  # translate by 40/60px on x/y axis, and scale to 50-70%, affects BBs
-    ])
-    seq_det = seq.to_deterministic()  # 保持坐标和图像同步改变，而不是随机
-    image_aug = seq_det.augment_images([img])[0]
-    bbs_aug = seq_det.augment_bounding_boxes([bbs])[0]
+                # 存储变化后的XML
+                change_xml_list_annotation(XML_DIR, name[:-4], new_bndbox_list,AUG_XML_DIR,epoch)
+                print(str(name[:-4]) + "_aug_" + str(epoch) + '.jpg')
+                new_bndbox_list = []
 
-    before = bbs.bounding_boxes
-    after = bbs_aug.bounding_boxes
-    print("BB : (%.4f, %.4f, %.4f, %.4f) -> (%.4f, %.4f, %.4f, %.4f)" % (
-        before[0].x1, before[0].y1, before[0].x2, before[0].y2,
-        after[0].x1, after[0].y1, after[0].x2, after[0].y2)
-          )
-
-    image_before = bbs.draw_on_image(img, thickness=2)
-    image_after = bbs_aug.draw_on_image(image_aug, thickness=2)
-    Image.fromarray(image_before).save("before.jpg")
-    Image.fromarray(image_after).save('after.jpg')
-
-    new_bndbox = []
-    new_bndbox_list = []
-    new_bndbox.append(int(bbs_aug.bounding_boxes[0].x1))
-    new_bndbox.append(int(bbs_aug.bounding_boxes[0].y1))
-    new_bndbox.append(int(bbs_aug.bounding_boxes[0].x2))
-    new_bndbox.append(int(bbs_aug.bounding_boxes[0].y2))
-    for arr in range(len(bbs_aug.bounding_boxes)):
-        new_bndbox_list.append([int(bbs_aug.bounding_boxes[arr].x1),int(bbs_aug.bounding_boxes[arr].y1),int(bbs_aug.bounding_boxes[arr].x2),int(bbs_aug.bounding_boxes[arr].y2)])
-    # 修改xml tree 并保存
-    change_xml_annotation(cmd, image_id, new_bndbox)
